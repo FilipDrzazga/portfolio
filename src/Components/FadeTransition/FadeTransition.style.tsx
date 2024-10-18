643bece4858c9c3e196cdda877c9c5575b1c45f6
Filip Drzazga <filip.drzazga@gmail.com>
2024-10-18 08:49:15 +0000

    animation for FadeTransition comp.

--- src/Components/FadeTransition/FadeTransition.style.tsx
+++ src/Components/FadeTransition/FadeTransition.style.tsx
@@ -1,20 +1,20 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
-const FadeTransitionBlockOne = styled(motion.div)`
+const FadeTransitionBlockOne = styled(motion.div)<{ $backgroundColor?: string; }>`
   position: absolute;
   top: 0;
   left: 0;
   width: 100vw;
   height: 10vh;
-  background-color: tomato;
+  background-color: ${props=>props.$backgroundColor};
 `;
 const SvgCurve = styled.svg`
   position: absolute;
-  top: 99%;
+  top: 0;
   left: 0;
   width: 100%;
-  height: 70px;
+  height: 50vh;
   fill: rgba(41, 41, 41, 0.1);
   stroke: none;
 `;
--- src/Components/FadeTransition/FadeTransition.tsx
+++ src/Components/FadeTransition/FadeTransition.tsx
@@ -1,65 +1,77 @@
-import { motion } from "framer-motion";
+import { delay, motion } from "framer-motion";
 
 import * as S from "./FadeTransition.style";
 
-const initialPath = `M 0 0 L ${window.innerWidth + 50} 0 Q ${window.innerWidth / 2} 140 -50 0 `;
-
-const targetPath = `M 0 0 L ${window.innerWidth} 0 Q ${window.innerWidth / 2} 0 0 0`;
+const initialPath = `M 0 0 L ${window.innerWidth} 0 Q ${window.innerWidth / 2} 0 0 0`;;
+const targetPath = `M 0 0 L ${window.innerWidth + 300} 0 Q ${window.innerWidth / 2} 500 -300 0 `;
+const finalPath = `M 0 0 L ${window.innerWidth} 0 Q ${window.innerWidth / 2} 0 0 0`;
 
 const blockTransitionVariants = {
   initial: {
     y: "calc(0% - 10vh)",
   },
-  open: {
-    y: window.innerHeight,
+  open: (idx:number)=> ({
+    y: '100vh',
     transition: {
-      duration: 1.5,
+      duration: 2,
       ease: [0.65, 0, 0.35, 1],
+      delay:.1 * idx
     },
-  },
-  close: {
+  }),
+  close:(idx:number)=> ({
     y: "calc(0% - 10vh)",
+    zIndex: idx * -1,
     transition: {
-      duration: 1.5,
+      duration: 2,
       ease: [0.65, 0, 0.35, 1],
-    },
+      delay:.1 * idx
   },
+  }),
 };
 
 const pathTransitionVariants = {
   initial: {
     d: targetPath,
   },
-  open: {
-    d: initialPath,
+  open:(idx:number)=>( {
+    d:[initialPath,targetPath,finalPath],
     transition: {
-      duration: 1.5,
+      duration: 2,
       ease: [0.65, 0, 0.35, 1],
     },
-  },
+  }),
   close: {
-    d: targetPath,
+    d:[finalPath,targetPath,initialPath],
     transition: {
-      duration: 2.2,
+      duration: 2,
       ease: [0.65, 0, 0.35, 1],
     },
   },
 };
 
+const pathArrayColors = ['#99BC85','#BFD8AF','#D4E7C5','#E1F0DA','white'];
+
 export const FadeTransition = () => {
   return (
-    <S.FadeTransitionBlockOne variants={blockTransitionVariants} initial="initial" animate="open" exit="close">
-      <S.SvgCurve>
-        <motion.path
-          variants={pathTransitionVariants}
-          initial="initial"
-          animate="open"
-          exit="close"
-          d={initialPath}
-          fill="tomato"
-        ></motion.path>
-      </S.SvgCurve>
-    </S.FadeTransitionBlockOne>
+    <>
+    {pathArrayColors.map((item:string,idx:number)=>{
+      return(
+        <S.FadeTransitionBlockOne $backgroundColor={item} custom={idx} variants={blockTransitionVariants} initial="initial" animate="open" exit="close">
+        <S.SvgCurve>
+          <motion.path
+          custom={idx}
+            variants={pathTransitionVariants}
+            initial="initial"
+            animate="open"
+            exit="close"
+            d={initialPath}
+            fill={item}
+          ></motion.path>
+        </S.SvgCurve>
+      </S.FadeTransitionBlockOne>
+      )
+    })}
+    </>
   );
 };
 
