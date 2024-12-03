449a1d49e925fd299c6cc85f5d28977480742a2c
Filip Drzazga <filip.drzazga@gmail.com>
2024-12-03 21:17:36 +0000

    added style for Navigation comp.

--- src/Components/Navigation/Navigation.styled.tsx
+++ src/Components/Navigation/Navigation.styled.tsx
@@ -1,48 +1,67 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const SectionNavigation = styled.section`
-    width:100%;
-    height:7vh;
-    padding:${({theme})=>theme.padding.small};
-    `
+  width: 100%;
+  height: 7vh;
+  padding: ${({ theme }) => theme.padding.small};
+  background-color: ${({ theme }) => theme.colors.primary};
+  backdrop-filter: blur(10px);
+`;
 const HeaderNavigation = styled.header`
-    width:100%;
-    height:100%;
-    display:flex;
-    `
+  width: 100%;
+  height: 100%;
+  display: flex;
+`;
 const TitleNavigation = styled.h1`
-    width:50%;
-    height:100%;
-    font-family:${({theme})=>theme.fontFamily.latoLight};
-    font-weight:${({theme})=>theme.fontWeight.regular};
-    font-size:0.7rem;
-    letter-spacing:0.1rem;
-    `
+  width: 50%;
+  height: 100%;
+  font-family: ${({ theme }) => theme.fontFamily.latoLight};
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  font-size: 0.7rem;
+  letter-spacing: 0.1rem;
+`;
 const NavNavigation = styled.nav`
-    width:50%;
-    height:100%;
-    `
+  width: 50%;
+  height: 100%;
+`;
 const UlNavigation = styled.ul`
-    width:100%;
-    height:100%;
-    display:flex;
-    justify-content:flex-end;
-    gap:1rem;
-    list-style:none;
-    `
-const LiNavigation = styled.ul`
-    font-family:${({theme})=>theme.fontFamily.latoLight};
-    font-weight:${({theme})=>theme.fontWeight.light};
-    font-size:${({theme})=>theme.fontSize.small};
-    `
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const LiNavigation = styled.li`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+`;
+const ANavigation = styled.a`
+  font-family: ${({ theme }) => theme.fontFamily.latoLight};
+  font-weight: ${({ theme }) => theme.fontWeight.light};
+  font-size: ${({ theme }) => theme.fontSize.small};
+  color: ${({ theme }) => theme.colors.secondary};
+  text-decoration: none;
+`;
+const ANavigationUnderline = styled(motion.div)`
+  position: absolute;
+  bottom: -0.1rem;
+  left: 0;
+  width: 100%;
+  height: 0.2rem;
+  border-radius: 0.1rem;
+  background-color: ${({ theme }) => theme.colors.accent};
+`;
 
-
-
-export 
-{   SectionNavigation,
-    HeaderNavigation,
-    TitleNavigation,
-    NavNavigation,
-    UlNavigation,
-    LiNavigation
+export {
+  SectionNavigation,
+  HeaderNavigation,
+  TitleNavigation,
+  NavNavigation,
+  UlNavigation,
+  LiNavigation,
+  ANavigation,
+  ANavigationUnderline,
 };
--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,21 +1,31 @@
-import * as S from './Navigation.styled';
+import { useState } from "react";
 
+import * as S from "./Navigation.styled";
 
-const Navigation = ()=>{
-    return(
-        <S.SectionNavigation>
-            <S.HeaderNavigation>
-                <S.TitleNavigation>FILIPDRZAZGA</S.TitleNavigation>
-                <S.NavNavigation>
-                    <S.UlNavigation>
-                        <S.LiNavigation>about</S.LiNavigation>
-                        <S.LiNavigation>work</S.LiNavigation>
-                        <S.LiNavigation>contact</S.LiNavigation>
-                    </S.UlNavigation>
-                </S.NavNavigation>
-            </S.HeaderNavigation>
-        </S.SectionNavigation>
-    );
+const tabs = ["about", "work", "contact"];
+
+const Navigation = () => {
+  const [isSelected, setIsSelected] = useState(tabs[0]);
+
+  return (
+    <S.SectionNavigation>
+      <S.HeaderNavigation>
+        <S.TitleNavigation>FILIPDRZAZGA</S.TitleNavigation>
+        <S.NavNavigation>
+          <S.UlNavigation>
+            {tabs.map((tab, index) => (
+              <S.LiNavigation key={index} onClick={() => setIsSelected(tab)}>
+                <S.ANavigation>{tab}</S.ANavigation>
+                {tab === isSelected && (
+                  <S.ANavigationUnderline key={index} layoutId="underline"></S.ANavigationUnderline>
+                )}
+              </S.LiNavigation>
+            ))}
+          </S.UlNavigation>
+        </S.NavNavigation>
+      </S.HeaderNavigation>
+    </S.SectionNavigation>
+  );
 };
 
 export default Navigation;
