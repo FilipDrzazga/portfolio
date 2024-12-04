692aafc8b9992543b1f5a3a00a8c491db0882b1d
Filip Drzazga <filip.drzazga@gmail.com>
2024-12-04 14:05:22 +0000

    added leva, and image animation based on texture

--- src/Components/ShaderImageMaterial/ImageMesh.tsx
+++ src/Components/ShaderImageMaterial/ImageMesh.tsx
@@ -1,7 +1,11 @@
 import { useRef, useMemo } from "react";
+import * as THREE from 'three';
+import { useFrame } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
+import { useControls } from 'leva';
 
 import image from "../../Images/face_original.jpg";
+import displacement from '../../Images/textures/Turbulence 4 - 512x512.png'
 
 const vertexShader = `
 
@@ -20,22 +24,46 @@ void main() {
 const fragmentShader = `
 
 varying vec2 vUv;
-uniform sampler2D u_Imagetexture;
+
+uniform sampler2D u_imageTexture;
+uniform sampler2D u_displacementTexture;
+uniform float u_progress;
 
 void main() {
 
-  vec4 imageTexture = texture(u_Imagetexture, vUv);
+  vec4 displacementTexture = texture(u_displacementTexture,vUv);
+  vec2 displacementUV = vec2(vUv.x,vUv.y);
+  displacementUV.y = mix(vUv.y,displacementTexture.r,u_progress);
+
+  vec4 imageTexture = texture(u_imageTexture, displacementUV);
 
   gl_FragColor = vec4(imageTexture);
 }`;
 
 const ShaderImageMaterial = () => {
   const imageTexture = useTexture(image);
-  const meshRef = useRef(null);
+  const displacementTexture = useTexture(displacement)
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  useFrame((state) => {
+    if(meshRef.current){
+      (meshRef.current.material as THREE.ShaderMaterial).uniforms.u_progress.value = progress;
+    }
+  });
+
+  const options = useMemo(() => {
+    return {
+      progress: { value: 0, min: 0, max: 1, step: 0.01 },
+    }
+  }, []);
+
+  const { progress } = useControls(options);
 
   const uniforms = useMemo(
     () => ({
-      u_Imagetexture: { value: imageTexture },
+      u_imageTexture: { value: imageTexture },
+      u_displacementTexture: { value: displacementTexture},
+      u_progress: {value:progress},
     }),
     [imageTexture]
   );
