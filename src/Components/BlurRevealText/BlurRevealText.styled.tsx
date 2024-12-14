e133e9c0d810bdaeb0b3cc4826c6348f479f797e
Filip Drzazga <filip.drzazga@gmail.com>
2024-12-14 01:45:11 +0000

    bluring text animation depends on scrollYPos --BlurRevealText comp.

--- src/Components/BlurRevealText/BlurRevealText.styled.tsx
+++ src/Components/BlurRevealText/BlurRevealText.styled.tsx
@@ -1,37 +1,37 @@
 import styled from "styled-components";
-import {motion} from 'motion/react'
+import { motion } from "motion/react";
 
 interface Props {
-    $height:number;
+  $height: number;
 }
 
 const DivStoryContainer = styled(motion.div)<Props>`
-position:sticky;
-top: ${({$height})=> `calc(45% - ${$height / 2}px)` };
-// top:50vh;
-display:flex;
-flex-wrap:wrap;
-justify-content:center;
-align-items:center;
-gap:0.2rem;
-color: ${({ theme }) => theme.colors.primary};
+  position: sticky;
+  margin-top: 20vh;
+  top: ${({ $height }) => `calc(45% - ${$height / 2}px)`};
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 0.2rem;
+  color: ${({ theme }) => theme.colors.primary};
 `;
 const CharactersContainer = styled(motion.div)`
-margin-bottom:-0.5rem;
-&[data-specialcontainer='true'] {
-margin-top:-0.35rem;
-}
+  margin-bottom: -0.5rem;
+  &[data-specialcontainer="true"] {
+    margin-top: -0.35rem;
+  }
 `;
 const SpanCharacters = styled(motion.span)`
   font-size: 1.8rem;
   font-family: ${({ theme }) => theme.fontFamily.latoLight};
   font-weight: ${({ theme }) => theme.fontWeight.light};
-  margin-left:-0.1rem;
-  &[data-specialchar='true'] {
+  margin-left: -0.1rem;
+  &[data-specialchar="true"] {
     font-size: 2rem;
     font-family: ${({ theme }) => theme.fontFamily.playfairBold};
     font-weight: ${({ theme }) => theme.fontWeight.bold};
   }
 `;
 
-export {DivStoryContainer, CharactersContainer, SpanCharacters};
+export { DivStoryContainer, CharactersContainer, SpanCharacters };
--- src/Components/BlurRevealText/BlurRevealText.tsx
+++ src/Components/BlurRevealText/BlurRevealText.tsx
@@ -1,110 +1,127 @@
-import { useRef, useEffect, useState } from 'react';
-import { type MotionValue, useMotionValueEvent} from 'motion/react';
+import { useRef, useEffect, useState } from "react";
+import { type MotionValue, useTransform } from "motion/react";
 
-import * as S from './BlurRevealText.styled';
-import { delay } from 'motion';
+import * as S from "./BlurRevealText.styled";
 
-const text1 = 'A'.split('');
-const text1special = 'self-taught'.split('');
-const text2 = 'and emerging'.split('');
-const text3 = 'creator,'.split('');
-const text2special = 'always'.split('');
-const text4 = 'trying to push'.split('');
-const text5 = 'the boundaries to bring'.split('');
-const text3special = 'cuzy'.split('');
-const text6 = 'ideas to'.split('');
-const text4special = 'web/app'.split('');
-const text7 = 'development.'.split('');
+const text1 = "A".split("");
+const text1special = "self-taught".split("");
+const text2 = "and emerging".split("");
+const text3 = "creator,".split("");
+const text2special = "always".split("");
+const text4 = "trying to push".split("");
+const text5 = "the boundaries".split("");
+const text6 = "to bring".split("");
+const text3special = "cuzy".split("");
+const text7 = "ideas to".split("");
+const text4special = "web/app".split("");
+const text8 = "development.".split("");
 
 const wordsObj = {
-    char1:text1,
-    special1:text1special,
-    char3:text2,
-    char4:text3,
-    special2:text2special,
-    char6:text4,
-    char7:text5,
-    special3:text3special,
-    char9:text6,
-    special410:text4special,
-    char11:text7
+  char1: text1,
+  special1: text1special,
+  char2: text2,
+  char3: text3,
+  special2: text2special,
+  char4: text4,
+  char5: text5,
+  cart6: text6,
+  special3: text3special,
+  char7: text7,
+  special410: text4special,
+  char8: text8,
 };
-
-const charContainerVariants = {
-    initial:{
-        opacity:0,
-    },
-    enter:(i:number)=>({
-        opacity:1,
-        transition:{
-            // delay: i * 0.01,
-            staggerChildren:0.1,
-        }
-    })
-};
-
-const charVariants = {
-    initial:{
-        opacity: 0,
-        filter: "blur(40px)",
-    },
-    enter:{
-        opacity: 1,
-        filter: "blur(0px)",
-        transition:{
-            duration:0.8
-        }
-    }
-}
 
 interface BlurRevealTextProps {
-     readonly scrollYProgress:MotionValue<number>
+  readonly scrollYProgress: MotionValue<number>;
 }
 
+const BlurRevealText = ({ scrollYProgress }: BlurRevealTextProps) => {
+  const DivStoryContainerRef = useRef<HTMLDivElement>(null);
+  const [divStoryContainerHeight, setdivStoryContainerHeight] = useState(0);
 
-const BlurRevealText = ({scrollYProgress}:BlurRevealTextProps)=>{
-const DivStoryContainerRef = useRef<HTMLDivElement>(null);
-const [divStoryContainerHeight, setdivStoryContainerHeight] = useState(0);
-const [isAnimationComplete, setIsAnimationComplete] = useState(0);
+  useEffect(() => {
+    if (DivStoryContainerRef.current) {
+      const height = DivStoryContainerRef.current.getBoundingClientRect().height;
+      setdivStoryContainerHeight(height);
+    }
+  }, []);
 
-    useMotionValueEvent(scrollYProgress, 'change', (latest)=>{
-        console.log(latest)
-    });
-
-    useEffect(()=>{
-        if(DivStoryContainerRef.current){
-            const height = DivStoryContainerRef.current.getBoundingClientRect().height;
-            setdivStoryContainerHeight(height);
-        };
-    },[])
-
-    return(
-    <S.DivStoryContainer $height={+divStoryContainerHeight} ref={DivStoryContainerRef}>        
-        {Object.entries(wordsObj).map(([key,charArr],id)=>(
-            <S.CharactersContainer 
-            onAnimationComplete={()=>{
-                if(id === isAnimationComplete && isAnimationComplete < 10){
-                    setIsAnimationComplete(prev=>prev + 1);
-                }
-            }}
-            custom={id} 
-            variants={charContainerVariants} 
-            initial='initial' 
-            animate={id <= isAnimationComplete ? 'enter' : 'initial'} 
-            data-specialcontainer={key.includes('special') ? 'true' : 'false'} 
-            key={key}>
-                {charArr.map((char,id)=>(
-                    <S.SpanCharacters 
-                    data-specialchar={key.includes('special') ? 'true' : 'false'} 
-                    key={id}
-                    variants={charVariants}
-                    >{char}
-                    </S.SpanCharacters>
-                ))}
-            </S.CharactersContainer>
-        ))}
+  return (
+    <S.DivStoryContainer $height={divStoryContainerHeight} ref={DivStoryContainerRef}>
+      {Object.entries(wordsObj).map(([key, charArr], id) => {
+        const start = id / 15; //delay each word by 1/11
+        const end = start + 1 / 12;
+        return (
+          <CharactersContainer
+            key={id}
+            keyValue={key}
+            data-specialcontainer={key.includes("special") ? "true" : "false"}
+            range={[start, end]}
+            progress={scrollYProgress}
+            charArr={charArr}
+          >
+            {charArr}
+          </CharactersContainer>
+        );
+      })}
     </S.DivStoryContainer>
-    );
+  );
 };
 
 export default BlurRevealText;
+
+// ///////////////////////////////////////////////////////////////////////////
+interface CharactersContainerProps {
+  readonly children: React.ReactNode;
+  readonly range: number[];
+  readonly progress: MotionValue<number>;
+  readonly charArr: string[];
+  readonly "data-specialcontainer"?: string;
+  readonly keyValue: string;
+}
+
+const CharactersContainer = ({ children, ...props }: CharactersContainerProps) => {
+  const { progress, range, charArr, keyValue } = props;
+  const amount = range[1] - range[0];
+  const step = amount / (Array.isArray(children) ? children.length : 1);
+  //   const opacity = useTransform(progress, range, [0, 1]);
+
+  return (
+    <S.CharactersContainer {...props}>
+      {charArr.map((char, id) => {
+        const start = range[0] + (step * id) / 11;
+        const end = range[0] + step * (id + 10);
+        return (
+          <SpanCharacters key={id} keyValue={keyValue} range={[start, end]} progress={progress}>
+            {char}
+          </SpanCharacters>
+        );
+      })}
+    </S.CharactersContainer>
+  );
+};
+
+// ///////////////////////////////////////////////////////////////////////////
+interface SpanCharactersProps {
+  readonly children: React.ReactNode;
+  readonly range: number[];
+  readonly progress: MotionValue<number>;
+  readonly keyValue: string;
+}
+
+const SpanCharacters = ({ children, ...props }: SpanCharactersProps) => {
+  const { progress, range, keyValue } = props;
+  const opacity = useTransform(progress, range, [0, 1]);
+  const blur = useTransform(progress, range, ["blur(40px)", "blur(0px)"]);
+  const scale = useTransform(progress, range, [0, 1]);
+
+  return (
+    <S.SpanCharacters
+      style={{ opacity, filter: blur, scale }}
+      data-specialchar={keyValue.includes("special") ? "true" : "false"}
+      {...props}
+    >
+      {children}
+    </S.SpanCharacters>
+  );
+};
