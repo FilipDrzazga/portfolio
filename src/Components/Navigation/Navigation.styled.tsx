5517f94f7b2814d0547d95c4513c6051e4d5bc4d
Filip Drzazga <filip.drzazga@gmail.com>
2025-01-20 22:11:23 +0000

    blury navigation, position fixed

--- src/Components/Navigation/Navigation.styled.tsx
+++ src/Components/Navigation/Navigation.styled.tsx
@@ -2,12 +2,15 @@ import styled from "styled-components";
 import { motion } from "motion/react";
 
 const SectionNavigation = styled.section`
+  position: fixed;
   width: 100%;
   height: 7vh;
   padding: ${({ theme }) => theme.padding.small};
-  background-color: ${({ theme }) => theme.colors.primary};
-  backdrop-filter: blur(10px);
-  z-index: 3;
+  background-color: transparent;
+  backdrop-filter: blur(5px);
+  z-index: 1000;
+  mask-image: linear-gradient(to bottom, black 55%, transparent 100%);
+  -webkit-mask-image: linear-gradient(to bottom, black 55%, transparent 100%);
 `;
 const HeaderNavigation = styled.header`
   width: 100%;
@@ -21,6 +24,7 @@ const TitleNavigation = styled.h1`
   font-weight: ${({ theme }) => theme.fontWeight.regular};
   font-size: 0.7rem;
   letter-spacing: -0.01rem;
+  color: ${({ theme }) => theme.colors.secondary};
 `;
 const NavNavigation = styled.nav`
   width: 50%;
