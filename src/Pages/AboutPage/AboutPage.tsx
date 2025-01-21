bf5db8a014433fcf7ead99acde8d06015a09d525
Filip Drzazga <filip.drzazga@gmail.com>
2025-01-21 15:35:04 +0000

    intersection-observer fire when scroll up or down

--- src/Pages/AboutPage/AboutPage.tsx
+++ src/Pages/AboutPage/AboutPage.tsx
@@ -19,7 +19,6 @@ import { OrbitControls } from '@react-three/drei';
 const AboutPage = () => {
   const SectionAboutHeroRef = useRef<HTMLDivElement>(null);
   const sectionAboutStoryRef = useRef<HTMLDivElement>(null);
-  const ref = useRef<HTMLDivElement>(null);
   // const sectionTransition = useRef<HTMLDivElement>(null);
   const imgRef = useRef<HTMLImageElement>(null);
   const imgRect = useRect(imgRef);
@@ -47,19 +46,41 @@ const AboutPage = () => {
 
   useEffect(() => {
     const callback = (entries: any[])=>{
+      let previousY = 0
+      let previousRatio = 0
       entries.forEach((entry: any) => {
-        console.log(entry);
-        setIsIntersecting(entry.isIntersecting);
+    const currentY = entry.boundingClientRect.y
+    const currentRatio = entry.intersectionRatio
+    const isIntersecting = entry.isIntersecting
+
+    if (currentY < previousY) {
+      if (currentRatio > previousRatio && isIntersecting) {
+        console.log("Scrolling down enter")
+        setIsIntersecting(true);
+      } else {
+        console.log("Scrolling down leave")
+        setIsIntersecting(false);
+      }
+    } else if (currentY > previousY && isIntersecting) {
+      if (currentRatio < previousRatio) {
+        console.log("Scrolling up leave")
+      } else {
+        console.log("Scrolling up enter")
+        setIsIntersecting(false);
+      }
+    }
+    previousY = currentY
+    previousRatio = currentRatio
       });
     };
     const options = {
-      threshold: 0.5,
-      rootMargin: "100px 0px 0px 200px" 
+      root:null,
+      threshold: [0,0.5],
+      rootMargin: "0px" 
     }
     const observer = new IntersectionObserver(callback,options);
 
     observer.observe(sectionAboutStoryRef.current!);
-    observer.observe(ref.current!)
 
     return () => observer.disconnect();
   }, []);
@@ -75,7 +96,7 @@ const AboutPage = () => {
         <OrbitControls />
         <ShaderImageMaterial imageRect={imgRect} scrollY={scrollY} />
       </Canvas>
-    <Navigation isInView={false} />
+    <Navigation isInView={isIntersecting} />
       <S.SectionAboutHero ref={SectionAboutHeroRef}>
         <S.HeaderAbout>
           <S.TitleAboutFirst>
@@ -91,11 +112,11 @@ const AboutPage = () => {
         </S.HeaderAbout>
         <ScrollToExplore/>
       </S.SectionAboutHero>
-      <S.SectionAboutStory ref={sectionAboutStoryRef}>
+      <S.SectionAboutStory ref={sectionAboutStoryRef} data-intersection='1'>
         <BounceSVG scrollYProgress={scrollYProgress} />
         <BlurRevealText scrollYProgress={scrollYProgress} />
       </S.SectionAboutStory>
-      <section ref={ref} style={{width:'100%', height:'200vh'}}>
+      <section style={{width:'100%', height:'200vh'}}>
 
       </section>
     </>
