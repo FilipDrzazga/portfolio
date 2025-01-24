75f6b09352b65f9e3fa6734876177ef30266dd29
Filip Drzazga <filip.drzazga@gmail.com>
2025-01-24 14:55:16 +0000

    added Leva options for fragment shader in ShaderAboutMe...

--- src/Components/ShaderAboutMeImageMaterial/ShaderAboutMeImageMaterial.tsx
+++ src/Components/ShaderAboutMeImageMaterial/ShaderAboutMeImageMaterial.tsx
@@ -4,6 +4,7 @@ import { useFrame } from "@react-three/fiber";
 import { useTexture } from "@react-three/drei";
 import { PageContext } from "../../context/PageContext";
 import { useScroll } from "framer-motion";
+import { useControls } from 'leva'
 
 import fragmentShader from './shaders/fragmentShader.glsl?raw';
 import vertexShader from "./shaders/vertexShader.glsl?raw";
@@ -14,7 +15,34 @@ const ShaderAboutMeImageMaterial = ()=>{
     const ctxPage = useContext(PageContext);
     const meshRef = useRef<THREE.Mesh>(null!);
 
+    const uniformsOptions = {
+      numSegments: {
+        value: 7.0,
+        min: 0.0,
+        max: 100.0,
+        step: 1.0,},
+        inputOutputRatio:{
+          value: 1.0,
+          min: 0.0,
+          max: 10.0,
+          step: 0.1,
+        },
+        overlap:{
+          value: 0.0,
+          min: 0.0,
+          max: 10.0,
+          step: 0.1,
+        },
+        light_strength:{
+          value: 0.1,
+          min: 0.0,
+          max: 10.0,
+          step: 0.1,
+        }
+    };
+
     const { scrollY } = useScroll();
+    const { numSegments, inputOutputRatio, overlap, light_strength } = useControls(uniformsOptions);
 
     const imageTexture = useTexture(image);
 
@@ -32,14 +60,24 @@ const ShaderAboutMeImageMaterial = ()=>{
         const uniforms = useMemo(
           () => ({
             u_imageTexture: { value: imageTexture},
+            u_numSegments:{value: 0.0},
+            u_inputOutputRatio:{value: 0.0},
+            u_overlap:{value: 0.0},
+            u_light_strength: { value: 0.0 },
           }),
           [imageTexture]
         );
 
         const updateShaderUniforms = useCallback(
-          (scrollYValue: number) => {
+          (scrollYValue: number, numSegments:number, inputOutputRatio:number,overlap:number, light_strength:number) => {
             if (!meshRef.current) return;
+            const shaderMaterial = meshRef.current.material as THREE.ShaderMaterial;
+            const { uniforms: shaderUniforms } = shaderMaterial;
       
+            shaderUniforms.u_numSegments.value = numSegments;
+            shaderUniforms.u_inputOutputRatio.value = inputOutputRatio;
+            shaderUniforms.u_overlap.value = overlap;
+            shaderUniforms.u_light_strength.value = light_strength;
             const targetY = scrollYValue + calculatedMeshPosition.topMeshPosition;
             const targetX = calculatedMeshPosition.leftMeshPosition;
       
@@ -49,7 +87,7 @@ const ShaderAboutMeImageMaterial = ()=>{
         );
 
         useFrame(() => {
-          updateShaderUniforms(scrollY.get());
+          updateShaderUniforms(scrollY.get(), numSegments, inputOutputRatio,overlap,light_strength);
         });
 
 
